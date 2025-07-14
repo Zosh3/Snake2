9868a769fe6b9a24699f5f9135cbb7cb91e43e06
Zosh3 <zosia.wilcock@outlook.com>
2025-07-14 13:15:43 +0100

    end of day 1

--- snake1.py
+++ snake1.py
@@ -12,6 +12,8 @@ It will draw a COLOUR_GREEN square on a black background and wait for 2 seconds 
 # Each colour is a tuple of COLOUR_RED, COLOUR_GREEN,  Blue  values.  Each value can range from 0 to 255.
 COLOUR_GREEN = (0, 255, 0)
 COLOUR_BLACK = (0, 0, 0)
+COLOUR_RED = (255, 0, 0)
+COLOUR_BLUE = (0, 0, 255)
 
 # Initialize Pygame
 pygame.init()
@@ -23,19 +25,34 @@ screen = pygame.display.set_mode((640, 480))
 
 # Set the title of the window
 pygame.display.set_caption("Python Snake Game")
+clock = pygame.time.Clock()
 
-# Fill the background with black
-screen.fill(COLOUR_BLACK)
+colour = COLOUR_GREEN  # Set the colour to COLOUR_GREEN
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        #print(event)
+        # If the user clicks the close button, exit the game
+        if event.type == pygame.QUIT:
+            game_over = True
 
-# Draw a COLOUR_GREEN square (the snake) at the center of the screen
-# The square is 20x20 pixels in size and is drawn at (320, 240) on the screen.
-# The coordinates are the top-left corner of the square.
-pygame.draw.rect(screen, COLOUR_GREEN, [320, 240, 20,20])
+        if event.type == pygame.KEYDOWN:
+            # If the user presses the escape key, exit the game
+            if event.key == pygame.K_b:
+                colour = COLOUR_BLUE
+            elif event.key == pygame.K_r:
+                colour = COLOUR_RED
 
-pygame.display.update()
+    # Fill the background with black
+    screen.fill(COLOUR_BLACK)
 
-# Wait for 2 seconds to see the drawn square
-# This is just to keep the window open for a while before quitting.
-pygame.time.delay(10000)
+    # Draw a COLOUR_GREEN square (the snake) at the center of the screen
+    # The square is 20x20 pixels in size and is drawn at (320, 240) on the screen.
+    # The coordinates are the top-left corner of the square.
+    pygame.draw.rect(screen, colour, [320, 240, 20,20])
+
+    pygame.display.update()
+    clock.tick(5)  # Limit the frame rate to 5 FPS
+
 
 pygame.quit()
